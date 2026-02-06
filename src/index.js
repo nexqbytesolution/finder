0eb97eb4f61e1afd580441659b64359026d7cadc
Umesh Joshi <uj19343@gmail.com>
2026-02-06 22:42:47 +0545

    new update

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import "dotenv/config";
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Server is running 🚀");
+});
+
+app.listen(3000, () => {
+  console.log(`Server started on port ${PORT}`);
+});
